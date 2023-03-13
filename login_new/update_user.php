7736815cf87010686b1bfc7fa059a5e4e4dee7a8
PEDRO GOMEZ <prgc105@gmail.com>
2023-03-13 14:24:08 -0600

    intento de actuali de contra pero sin exito

--- login_new/update_user.php
+++ login_new/update_user.php
@@ -5,10 +5,27 @@
     $username = $_POST["user"];
     $password = $_POST["password"];
 
+   
+
+    $query = mysqli_query($conn, "SELECT contra FROM login where contra='.$password' and id='user_id'");
+    while ($row = mysqli_fetch_object($query)) {
+        $row->$nombre1;
+    }
+ echo $sql;
+if($password == $nombre1 ){
+    $sql = "UPDATE login set 
+    nombre = '".$name."', usuario='".$username."' where id=".$user_id;
+
+    echo "sin contra modi";
+} else {
     $password = md5($password."cursophp");
 
     $sql = "UPDATE login set 
     nombre = '".$name."', usuario='".$username."', contra='".$password."' where id=".$user_id;
+
+echo "con contra modi";
+}
+
 
     
     if ($query = mysqli_query($conn, $sql))
